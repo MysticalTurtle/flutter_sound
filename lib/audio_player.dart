3ca394a8bd504611df7f4321666ad9fc24168c41
edsonsalvador2014@gmail.com <edsonsalvador2014@gmail.com>
2022-01-05 21:02:28 -0500

    Reproducir audio

--- lib/audio_player.dart
+++ lib/audio_player.dart
@@ -1,13 +1,40 @@
+import 'dart:ffi';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter_sound_lite/public/flutter_sound_player.dart';
 
 class SoundPlayer {
   FlutterSoundPlayer? _audioPlayer;
   final audioPath = "ejemplo.aac";
+  bool get isPlaying => _audioPlayer!.isPlaying;
 
-  Future play() async {
+  Future init() async {
+    _audioPlayer = FlutterSoundPlayer();
+    await _audioPlayer!.openAudioSession();
+  }
+
+  void dispose() {
+    _audioPlayer!.closeAudioSession();
+   _audioPlayer = null; 
+  }
+
+  Future play(VoidCallback whenFinished) async {
     await _audioPlayer!.startPlayer(
-      fromURI: audioPath,  
+      fromURI: audioPath,
+      whenFinished: whenFinished,
     );
+  }
+
+  Future stop() async {
+    await _audioPlayer!.stopPlayer();
+  }
+
+  Future togglePlaying({required VoidCallback whenFinished}) async {
+    if (_audioPlayer!.isStopped) {
+      play(whenFinished);
+    } else {
+      stop();
+    }
   }
 
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:sonido/audio_player.dart';
 import 'audio_recorder.dart';
+
+// from : https://www.youtube.com/watch?v=64xJO0urK9E
+// from : https://www.youtube.com/watch?v=-Q5c-E63kfI&t=134s
 
 void main() {
   runApp(const MyApp());
@@ -14,17 +18,20 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   final recorder = SoundRecorder();
+  final player = SoundPlayer();
 
   @override
   void initState() {
     super.initState();
     recorder.init();
+    player.init();
   }
 
   @override
   void dispose() {
     super.dispose();
     recorder.dispose();
+    player.dispose();
   }
 
   @override
@@ -36,15 +43,24 @@ class _MyAppState extends State<MyApp> {
       ),
       home: Scaffold(
         appBar: AppBar(),
-        body: Center(child: buildStart(),),
+        body: Column(
+          children: [
+            Center(
+              child: recorButton(),
+            ),
+            Center(
+              child: playButton(),
+            )
+          ],
+        ),
       ),
     );
   }
 
-  Widget buildStart() {
+  Widget recorButton() {
     final bool isRecording = recorder.isRecording;
     // final bool isRecording = false;
-    final text = isRecording ? "Pausar" : "Iniciar";
+    final text = isRecording ? "Detener" : "Grabar";
     final icon = isRecording ? Icons.stop : Icons.mic;
     return ElevatedButton.icon(
       icon: Icon(icon),
@@ -54,5 +70,18 @@ class _MyAppState extends State<MyApp> {
         setState(() {});
       },
     );
+  }
+
+  Widget playButton() {
+    final isPlaying = player.isPlaying;
+    final icon = isPlaying? Icons.stop : Icons.play_arrow;
+    final text = isPlaying? "Detener" : "Reproducir";
+    return ElevatedButton.icon(
+        onPressed: () async {
+          await player.togglePlaying(whenFinished: () => setState(() {}));
+          setState(() {});
+        },
+        icon: Icon(icon),
+        label: Text(text));
   }
 }
